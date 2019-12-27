e2f7fb7c4f65b6070047f1728ab40a599c39678c
georgematos <georgemattos@gmail.com>
2019-12-27 17:12:48 -0300

    implement calc sum 2 values

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,20 +1,38 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
 	@FXML
-	private Button btTest;
+	private TextField txtNumber1;
+	@FXML
+	private TextField txtNumber2;
+	@FXML
+	private Label labelResult;
+	@FXML
+	private Button btSum;
 	
 	@FXML
-	public void onBtTestAction() {
+	public void onBtSumAction() {
 		
-		Alerts.showAlert("Alert title", null, "Button Clicked", AlertType.INFORMATION);
-	
+		Locale.setDefault(Locale.US);
+		try {
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double sum = number1 + number2;
+			labelResult.setText(String.format("%.2f", sum));
+		} catch (Exception e) {
+			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 	
 }
